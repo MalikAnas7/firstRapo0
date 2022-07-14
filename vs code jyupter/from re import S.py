4700e3cacbe4a1f19ad82dbff01412e67bddad08
malik <manaswaris@gmail.com>
2022-07-13 21:37:59 -0700

    Python Basics
    basic concepts of python.

---
+++ vs code jyupter/from re import S.py
@@ -0,0 +1,4 @@
+from re import S
+import pandas as pd
+s=pd.Series([13579])
+s
---
+++ vs code jyupter/panda.py
@@ -0,0 +1,7 @@
+from matplotlib import dates
+import pandas as pd
+from regex import S
+# g=pd.Series([1,3,5,7])
+# print(g)
+dates=pd.date_range("20130101",periods=6)
+dates
